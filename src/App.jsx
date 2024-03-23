557d9c9fc79297793d21d9f19e21a3a20742db76
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2024-03-24 00:46:52 +0600

    react-career-hub

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    
+      <h1 className='text-3xl bg-red-500 text-center'>Vite + React</h1>
+    
+    </>
+  )
+}
+
+export default App
